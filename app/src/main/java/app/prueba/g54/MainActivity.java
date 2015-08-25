e9f937c0878389c95f440d42b32fc4f823b164ba
cristian_dev <cristian_dev@pmovil.com.ar>
2015-08-25 18:09:30 -0300

    cambios

--- app/src/main/java/app/prueba/g54/MainActivity.java
+++ app/src/main/java/app/prueba/g54/MainActivity.java
@@ -1,11 +1,12 @@
 package app.prueba.g54;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    String pregunta = "esta";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,25 +14,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    //Método botón clickeado.
+    public void submitText(View view) {
+        displaypregunta(pregunta);
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private void displaypregunta(String pregunta) {
+        TextView preguntaTextView = (TextView) findViewById(R.id.pregunta);
+        preguntaTextView.setText("aja");
     }
+
 }
