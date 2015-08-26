d701b4f1bb1168fb5a6556847be84d0f59a8dc84
cristian_dev <cristian_dev@pmovil.com.ar>
2015-08-26 17:32:00 -0300

    animación botones

--- app/src/main/java/app/prueba/g54/MainActivity.java
+++ app/src/main/java/app/prueba/g54/MainActivity.java
@@ -1,29 +1,69 @@
 package app.prueba.g54;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    String pregunta = "esta";
+public class MainActivity extends Activity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //más animaciones como: final Animation animShake = AnimationUtils.loadAnimation(this, R.anim.shake);
+        final Animation animAlpha = AnimationUtils.loadAnimation(this, R.anim.anim_alpha);
+
+        //llamando los botones
+        ImageButton btnSmile = (ImageButton) findViewById(R.id.button_smile);
+        ImageButton btnRegular = (ImageButton) findViewById(R.id.button_regular);
+        ImageButton btnNegative = (ImageButton) findViewById(R.id.button_negative);
+
+        //onclick
+        btnSmile.setOnClickListener(new ImageButton.OnClickListener()
+        {
+            @Override
+            public void onClick(View arg0) {
+                arg0.startAnimation(animAlpha);
+                displaypregunta (getString(R.string.pregunta_segunda));
+                }
+        });
+
+        btnRegular.setOnClickListener(new ImageButton.OnClickListener()
+        {
+            @Override
+            public void onClick(View arg0) {
+                arg0.startAnimation(animAlpha);
+                displaypregunta3(getString(R.string.pregunta_tercera));
+                }
+        });
+
+        btnNegative.setOnClickListener(new ImageButton.OnClickListener()
+        {
+            @Override
+            public void onClick(View arg0) {
+                arg0.startAnimation(animAlpha);
+                displaypregunta1 (getString(R.string.pregunta_primera));
+                }
+        });
+
     }
-
-    //Método botón clickeado.
-    public void submitText(View view) {
-        displaypregunta(pregunta);
-
-    }
-
+    //mostrar respuestas
     private void displaypregunta(String pregunta) {
         TextView preguntaTextView = (TextView) findViewById(R.id.pregunta);
-        preguntaTextView.setText("aja");
+        preguntaTextView.setText(R.string.pregunta_segunda);
     }
-
+    private void displaypregunta3(String pregunta) {
+        TextView preguntaTextView = (TextView) findViewById(R.id.pregunta);
+        preguntaTextView.setText(R.string.pregunta_tercera);
+    }
+    private void displaypregunta1(String pregunta) {
+        TextView preguntaTextView = (TextView) findViewById(R.id.pregunta);
+        preguntaTextView.setText(R.string.pregunta_primera);
+    }
 }
